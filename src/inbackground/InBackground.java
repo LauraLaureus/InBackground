9f98f4d078ff712751c8b78c1ab1d2806fa96995
Laura <l4depende@gmail.com>
2015-11-17 09:04:40 +0000

    Code

---
+++ src/inbackground/InBackground.java
@@ -0,0 +1,13 @@
+package inbackground;
+
+/**
+ *
+ * @author lala
+ */
+public class InBackground {
+
+    public static void main(String[] args) {
+        new MainFrame().setVisible(true);
+    }
+    
+}
